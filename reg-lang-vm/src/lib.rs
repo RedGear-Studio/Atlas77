3a9322d20dc8d484e0a5237cf13480dfb260950d
Gipson62 <J.H.Gipson62@gmail.com>
2023-01-03 15:19:32 +0100

    Add Equalities + Jump Instructions
    Add the equalities sign based on 2 registers + add the `equal_flag` register to store the latest result of a boolean expression. Add the JMP | JPMF | JMPB to jump in the code based on a value store in a certain register (to allow 32bits numbers to jump)

--- reg-lang-vm/src/lib.rs
+++ reg-lang-vm/src/lib.rs
@@ -1,12 +1,19 @@
+#![allow(unused, dead_code)]
 pub mod opcode;
 use opcode::*;
 
-#[allow(unused)]
+#[derive(Debug)]
 pub struct RegLangVM {
+    /// Array that simulates having hardware registers
     registers: [i32; 32],
+    /// Program counter that tracks which byte is being executed
     program_counter: usize,
+    /// The bytecode of the program being run
     program: Vec<u8>,
+    /// Contains the remainder of modulo division ops
     remainder: u32,
+    /// Contains the result of the last comparison operation
+    equal_flag: bool,
 }
 impl RegLangVM {
     pub fn new(program: Vec<u8>) -> Self {
@@ -15,6 +22,7 @@ impl RegLangVM {
             program_counter: 0,
             program,
             remainder: 0,
+            equal_flag: false,
         }
     }
     /// Loops as long as instructions can be executed.
@@ -65,6 +73,61 @@ impl RegLangVM {
                 let register2 = self.registers[self.next_8_bits() as usize];
                 self.registers[self.next_8_bits() as usize] = register1 / register2;
                 self.remainder = (register1 % register2) as u32;
+            },
+            OpCode::JMP => {
+                let value = self.registers[self.next_8_bits() as usize];
+                self.program_counter = value as usize;
+            },
+            OpCode::JMPF => {
+                let value = self.registers[self.next_8_bits() as usize];
+                self.program_counter += value as usize;
+            },
+            OpCode::JMPB => {
+                let value = self.registers[self.next_8_bits() as usize];
+                self.program_counter -= value as usize;
+            },
+            OpCode::EQ => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 == register2;
+                self.next_8_bits();
+            },
+            OpCode::NEQ => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 != register2;
+                self.next_8_bits();
+            },
+            OpCode::GT => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 > register2;
+                self.next_8_bits();
+            },
+            OpCode::LT => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 < register2;
+                self.next_8_bits();
+            },
+            OpCode::GTE => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 >= register2;
+                self.next_8_bits();
+            },
+            OpCode::LTE => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.equal_flag = register1 <= register2;
+                self.next_8_bits();
+            },
+            OpCode::JMPE => {
+                let register = self.next_8_bits() as usize;
+                let target = self.registers[register];
+                if self.equal_flag {
+                    self.program_counter = target as usize;
+                }
             }
             _ => {
                 println!("Unknown opcode encountered");
@@ -89,4 +152,10 @@ impl RegLangVM {
         self.program_counter += 2;
         return result;
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
 }
--- reg-lang-vm/src/opcode.rs
+++ reg-lang-vm/src/opcode.rs
@@ -12,6 +12,26 @@ pub enum OpCode {
     SUB,
     /// Divide two registers and store the result in a third register `DIV REGISTER1 REGISTER2 REGISTER3` and store the remainder in the remainder register
     DIV,
+    /// Jump to a register `JMP REGISTER`
+    JMP,
+    /// Jump forward to a certain amount of bytes `JMPF NUMBER`
+    JMPF,
+    /// Jump backward to a certain amount of bytes `JMPB NUMBER`
+    JMPB,
+    /// Equal to `EQ REGISTER1 REGISTER`
+    EQ,
+    /// Not equal to `NEQ REGISTER1 REGISTER2`
+    NEQ,
+    /// Greater than `GT REGISTER1 REGISTER2`
+    GT,
+    /// Less than `LT REGISTER1 REGISTER2`
+    LT,
+    /// Greater than or equal to `GTE REGISTER1 REGISTER2`
+    GTE,
+    /// Less than or equal to `LTE REGISTER1 REGISTER2`
+    LTE,
+    /// Jump if equal to `JMPE REGISTER1 REGISTER2`
+    JMPE,
     /// Illegal opcode
     ILG,
 }
@@ -24,7 +44,17 @@ impl From<u8> for OpCode {
             3 => return OpCode::MUL,
             4 => return OpCode::SUB,
             5 => return OpCode::DIV,
-            _ => return OpCode::ILG
+            6 => return OpCode::JMP,
+            7 => return OpCode::JMPF,
+            8 => return OpCode::JMPB,
+            9 => return OpCode::EQ,
+            10 => return OpCode::NEQ,
+            11 => return OpCode::GT,
+            12 => return OpCode::LT,
+            13 => return OpCode::GTE,
+            14 => return OpCode::LTE,
+            15 => return OpCode::JMPE,
+            _ => return OpCode::ILG,
         }
     }
 }
