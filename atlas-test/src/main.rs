29a02f91a420d044a38079cef18c9d283edf93c1
Gipson62 <J.H.Gipson62@gmail.com>
2023-11-30 20:11:10 +0100

    Add the new parser and typechecker

--- atlas-test/src/main.rs
+++ atlas-test/src/main.rs
@@ -4,14 +4,12 @@ pub mod simple_visitor;
 pub mod simple_cli;
 
 use clap::Parser as ClapParser;
-use atlas_lexer::{Lexer, AtlasLexer};
 
 use codespan_reporting::files::{SimpleFile, SimpleFiles};
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 use codespan_reporting::diagnostic::{Diagnostic, Label};
 use atlas_span::Spanned;
-use atlas_parser::{AtlasParser, Parser};
-use atlas_parser_interpreter::ASTInterpreter;
+//use atlas_parser_interpreter::ASTInterpreter;
 use atlas_utils::{Visitor, Expression, Value};
 
 use std::time::Instant;
@@ -26,53 +24,13 @@ struct Args {
 
 fn main() { 
 
-    let number_of_try = 10000;
+    let number_of_try = 500;
 
     let start = Instant::now();
 
     let path = "C:\\Users\\JHGip\\OneDrive\\Documents\\GitHub\\Atlas77\\atlas-test\\test.atlas";
     for _ in 0..number_of_try {
-        let mut lexer = AtlasLexer::new_with_path(path);
-        let res = lexer.tokenize();
-        //println!("{:?}", res);
-        if res.err.len() > 0 {
-            println!("{} errors", res.err.len());
-                    
-            let mut file = SimpleFiles::new();
-            let file_id = file.add(path, std::fs::read_to_string(path).unwrap());
-
-            for err in res.err.iter() {
-                let diagnostic= Diagnostic::error()
-                    .with_message(err.to_string())
-                    .with_code("Something")
-                    .with_labels(vec![
-                        Label::primary(file_id, err.start()..err.end()).with_message(err.to_string()),
-                    ])
-                    .with_notes(vec!["Notes".to_string()]);
-                
-                let writer = StandardStream::stderr(ColorChoice::Always);
-                let config = codespan_reporting::term::Config::default();
-                
-
-                if let Ok(_c) = codespan_reporting::term::emit(&mut writer.lock(), &config, &file, &diagnostic) {
-                    
-                };
-            }   
-        }
-
-        let mut parser = AtlasParser::new_with_path(path, res.tokens);
-        let res = parser.parse();
-        //println!("{:?}", res);
-
-        let mut visitor = ASTInterpreter::new();
-
-        let mut exprs = Vec::new();
-        for expr in res.iter() {
-            exprs.push(expr as &dyn Expression);
-        }
-
-        let res = visitor.evaluate(exprs);
-        //println!("{:?}", res);
+        atlas_parser::parse(path);
     }
 
     let elapsed = start.elapsed();
--- compiler/atlas_parser/src/lib.rs
+++ compiler/atlas_parser/src/lib.rs
@@ -1,1 +1,36 @@
+extern crate lalrpop_util;
+
 pub mod grammar;
+pub mod type_check;
+pub mod macros;
+pub mod ast {
+    pub use atlas_parser_ast::*;
+}
+
+pub fn parse(path: &'static str) {
+    match std::fs::read_to_string(path) {
+        Ok(txt) => {
+    
+            let parser = grammar::ProgramParser::new();
+            let res = grammar::ProgramParser::parse(
+                &parser,
+                &txt
+            );
+            match res {
+                Ok(decls) => {
+                    /*for decl in &decls {
+                        println!("{}", decl)
+                    }*/
+                    let mut ir_context = type_check::IRContext::new(decls);
+                    match ir_context.type_check() {
+                        Ok(_) => {/*println!("Type checked successfully")*/},
+                        Err(e) => println!("{:?}", e)
+                    }
+                    //println!("{:?}", ir_context);x
+                },
+                Err(e) => println!("{:?}", e)
+            }
+        },    
+        Err(e) => println!("{:?}", e)
+    }
+}
