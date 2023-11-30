d44d5b6328335f05b34b8768d0e581d8767b86ff
Gipson62 <J.H.Gipson62@gmail.com>
2023-11-30 19:21:49 +0100

    Commit full of errors

--- compiler/atlas_utils/src/lib.rs
+++ compiler/atlas_utils/src/lib.rs
@@ -35,7 +35,7 @@ pub trait Visitor {
     fn register_variable(&mut self, name: String, value: Value) -> Result<(), String>;
 }
 
-pub trait Expression {
+pub trait Expression : std::fmt::Debug {
     fn evaluate(&self, visitor: &mut dyn Visitor) -> Value;
 }
 
@@ -515,13 +515,13 @@ impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use Value::*;
         match self {
-            Int64(i) => write!(f, "{}_int64", i),
-            Int32(i) => write!(f, "{}_int32", i),
-            UInt64(u) => write!(f, "{}_uint64", u),
-            UInt32(u) => write!(f, "{}_uint32", u),
-            Float64(fl) => write!(f, "{}_float64", fl),
-            Float32(fl) => write!(f, "{}_float32", fl),
-            Bool(i) => write!(f, "{}_bool", i),
+            Int64(i) => write!(f, "{}_i64", i),
+            Int32(i) => write!(f, "{}_i32", i),
+            UInt64(u) => write!(f, "{}_u64", u),
+            UInt32(u) => write!(f, "{}_u32", u),
+            Float64(fl) => write!(f, "{}_f64", fl),
+            Float32(fl) => write!(f, "{}_f32", fl),
+            Bool(i) => write!(f, "{}", i),
             StringValue(s) => write!(f, "\"{}\"", s),
             Char(c) => write!(f, "'{}'", c),
             Array(arr) => write!(f, "[{}]", arr.iter().map(|v| v.to_string()).collect::<Vec<String>>().join(",")),
@@ -551,13 +551,13 @@ mod tests {
         for v in values.iter() {
             println!("{}", v);
         }
-        assert_eq!(values[0].to_string(), "0_int64");
-        assert_eq!(values[1].to_string(), "0_int32");
-        assert_eq!(values[2].to_string(), "0_uint64");
-        assert_eq!(values[3].to_string(), "0_uint32");
-        assert_eq!(values[4].to_string(), "0_float64");
-        assert_eq!(values[5].to_string(), "0_float32");
-        assert_eq!(values[6].to_string(), "true_bool");
+        assert_eq!(values[0].to_string(), "0_i64");
+        assert_eq!(values[1].to_string(), "0_i32");
+        assert_eq!(values[2].to_string(), "0_u64");
+        assert_eq!(values[3].to_string(), "0_u32");
+        assert_eq!(values[4].to_string(), "0_f64");
+        assert_eq!(values[5].to_string(), "0_f32");
+        assert_eq!(values[6].to_string(), "true");
         assert_eq!(values[7].to_string(), "\"Hello World!\"");
         assert_eq!(values[8].to_string(), "[]");
         assert_eq!(values[9].to_string(), "undefined");
